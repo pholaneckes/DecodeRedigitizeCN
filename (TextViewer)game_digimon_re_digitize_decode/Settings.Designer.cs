f66922123fca755630f101dc83b74a27d90394b9
pholaneckes <114641324+pholaneckes@users.noreply.github.com>
2024-09-06 20:08:45 +0800

    Add files via upload

---
+++ (TextViewer)game_digimon_re_digitize_decode/Settings.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace game_digimon_re_digitize_decode
+{
+    partial class Settings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.grpProperties = new System.Windows.Forms.GroupBox();
+            this.txtPlayerName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtModeName = new System.Windows.Forms.TextBox();
+            this.grpProperties.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnOK
+            // 
+            this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOK.Location = new System.Drawing.Point(170, 133);
+            this.btnOK.Margin = new System.Windows.Forms.Padding(4);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(112, 32);
+            this.btnOK.TabIndex = 3;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(291, 133);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(112, 32);
+            this.btnCancel.TabIndex = 4;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // grpProperties
+            // 
+            this.grpProperties.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grpProperties.Controls.Add(this.txtPlayerName);
+            this.grpProperties.Controls.Add(this.label1);
+            this.grpProperties.Font = new System.Drawing.Font("宋体", 19F);
+            this.grpProperties.Location = new System.Drawing.Point(17, 13);
+            this.grpProperties.Margin = new System.Windows.Forms.Padding(4);
+            this.grpProperties.Name = "grpProperties";
+            this.grpProperties.Padding = new System.Windows.Forms.Padding(9, 8, 9, 8);
+            this.grpProperties.Size = new System.Drawing.Size(386, 112);
+            this.grpProperties.TabIndex = 2;
+            this.grpProperties.TabStop = false;
+            this.grpProperties.Text = "设置";
+            this.grpProperties.Enter += new System.EventHandler(this.grpProperties_Enter);
+            // 
+            // txtPlayerName
+            // 
+            this.txtPlayerName.Location = new System.Drawing.Point(132, 44);
+            this.txtPlayerName.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPlayerName.MaxLength = 10;
+            this.txtPlayerName.Name = "txtPlayerName";
+            this.txtPlayerName.Size = new System.Drawing.Size(229, 51);
+            this.txtPlayerName.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(28, 38);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 48);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "名称:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtModeName
+            // 
+            this.txtModeName.Location = new System.Drawing.Point(0, 0);
+            this.txtModeName.Name = "txtModeName";
+            this.txtModeName.Size = new System.Drawing.Size(100, 28);
+            this.txtModeName.TabIndex = 0;
+            // 
+            // Settings
+            // 
+            this.AcceptButton = this.btnOK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(422, 181);
+            this.Controls.Add(this.grpProperties);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Settings";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "耧上设定";
+            this.Load += new System.EventHandler(this.Settings_Load);
+            this.grpProperties.ResumeLayout(false);
+            this.grpProperties.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.GroupBox grpProperties;
+        private System.Windows.Forms.TextBox txtPlayerName;
+        private System.Windows.Forms.TextBox txtModeName;
+        private System.Windows.Forms.Label label1;
+    }
+}
